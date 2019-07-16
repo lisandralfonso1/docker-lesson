028c3553465d93d7faff2c24c7658129c984f28d
Scott Hurlow <Scottyhurlow@gmail.com>
2019-07-16 13:37:48 -0700

    removes solution

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 // Change the following connection url to refer to the docker database container
-mongoose.connect('mongodb://database:27017/users', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/users', { useNewUrlParser: true })
 
 const userSchema = new mongoose.Schema({
   name: String
