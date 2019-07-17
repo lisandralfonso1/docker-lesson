8ef4fe0c10ca9c9736eecaeb34ffd742ae73a164
Maria L Alfonso <lisandralfonso@yahoo.com>
2019-07-17 14:38:31 -0700

    completed

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node 
+
+WORKDIR /app
+
+COPY . /app
+
+RUN npm install
+
+EXPOSE 3001
+
+CMD [ "npm", "start" ]
+
+
--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 const { Users } = require('./db')
 const app = express()
 
